bfd589f98e4a1c033fdb18295fa50b0805a9c55c
F4r3n <guillaume_k@hotmail.fr>
2015-05-23 22:56:59 +0100

    Movie get poster

--- src/com/example/movie/MainActivity.java
+++ src/com/example/movie/MainActivity.java
@@ -1,18 +1,5 @@
 package com.example.movie;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
--- src/com/example/movie/Movie.java
+++ src/com/example/movie/Movie.java
@@ -1,14 +1,16 @@
 package com.example.movie;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Movie {
-	
-	//private Map<String, String > _dico = new HashMap<String, String>();
+
+	// private Map<String, String > _dico = new HashMap<String, String>();
 	private String _request;
 	private JSONObject _parser;
-	
+	private String _name;
+
 	public Movie(String request) {
 		_request = request;
 		try {
@@ -16,22 +18,23 @@ public class Movie {
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
+		getName();
 	}
-	
-	
-	
-	public String getTitle() {
-		String result ="";
+
+	private void getName() {
 		try {
-			result = _parser.getString("Title");
+			_name = _parser.getString("Title");
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		return result;
 	}
-	
+
+	public String getTitle() {
+		return _name;
+	}
+
 	public String getPlot() {
-		String result ="";
+		String result = "";
 		try {
 			result = _parser.getString("Plot");
 		} catch (JSONException e) {
@@ -39,8 +42,24 @@ public class Movie {
 		}
 		return result;
 	}
-	
-	
-	
+
+	public String getPoster() {
+		String result = "";
+		try {
+			result = _parser.getString("Poster");
+			if (result.equals("")) {
+				result = ObjectRequest
+						.sendRequest("http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="
+								+ _name + "poster");
+				JSONArray jArray = _parser.getJSONArray("results");
+				JSONObject oneObject = jArray.getJSONObject(0);
+				result = oneObject.getString("url");
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return result;
+
+	}
 
 }
